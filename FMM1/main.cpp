b9fd8a806dd0f22b54b0f7bf62115e0f68ff5f94
fimi666 <f.michlik@gmail.com>
2014-10-14 12:13:22 +0200

    initial commit

---
+++ FMM1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FMM1
+//
+//  Created by fimi on 14/10/14.
+//  Copyright (c) 2014 Filip Michlik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
