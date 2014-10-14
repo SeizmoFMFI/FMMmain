1c5c7dac5dd8bee7b34d8814d62808c7feba9a8c
fimi666 <f.michlik@gmail.com>
2014-10-14 12:22:11 +0200

    ~no changes

--- FMM1/main.cpp
+++ FMM1/main.cpp
@@ -1,18 +1,48 @@
 //
 //  main.cpp
-//  FMM1
+//  FastMarchingMethod
 //
-//  Created by fimi on 14/10/14.
+//  Created by fimi on 30/09/14.
 //  Copyright (c) 2014 Filip Michlik. All rights reserved.
 //
 
 #include <iostream>
 
+#include "FastMarchingMethod.h"
+#include "VelocityModel.h"
+
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int dx, dy, dz, sx, sy, sz;
+    //prepare MODEL
+    dx = 100;
+    dy = 100;
+    dz = 100;
+    
+    Dimension *dimension = new Dimension(dx, dy, dz);
+    VelocityModel *vm = new VelocityModel(dimension);
+    
+    cout << "MODEL-dimensions: " << vm->dimension->dx << " x " << vm->dimension->dy << " x " << vm->dimension->dz << endl;
+    
+    //prepare SOURCE POSITION
+    sx = 50;
+    sy = 50;
+    sz = 50;
+    GridPosition *source = new GridPosition(sx, sy, sz);
+    
+    //instantiate FMM
+    FastMarchingMethod *fmm = new FastMarchingMethod(vm, source);
+    
+    //RUN CALCULATIONS
+    
+    //release memory
+    delete fmm;
+    delete source;
+    delete vm;
+    delete dimension;
+    
     return 0;
 }
 
